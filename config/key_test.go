5362d8b65468ca83b3af1801389cf1f91f68c912
Andrey Pozolotin <posandrey@gmail.com>
2021-02-04 01:12:33 +0300

    Added test

---
+++ config/key_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRequiredValidate(t *testing.T) {
+	testCases := []struct{
+		inputValue interface{}
+		fieldName string
+		expectedError string
+	} {
+		{
+			inputValue:    nil,
+			fieldName:     "someVal",
+			expectedError: "value 'someVal' is required and cannot be empty",
+		},
+		{
+			inputValue:    "",
+			fieldName:     "someVal",
+			expectedError: "value 'someVal' is required and cannot be empty",
+		},
+		{
+			inputValue:    "a",
+			fieldName:     "someVal",
+		},
+	}
+
+	for _, testCase := range testCases {
+		actualErr := RequiredValidate(testCase.fieldName, testCase.inputValue)
+		if testCase.expectedError == "" {
+			assert.NoError(t, actualErr)
+			continue
+		}
+
+		assert.EqualError(t, actualErr, testCase.expectedError)
+	}
+}
