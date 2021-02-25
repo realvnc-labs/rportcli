e42f71fbe77d74d2b99c6f304f3cdbeea2638f13
Andrey Pozolotin <posandrey@gmail.com>
2021-02-25 17:45:16 +0100

    Interrupt password input

--- internal/pkg/utils/prompt.go
+++ internal/pkg/utils/prompt.go
@@ -12,19 +12,35 @@ type PromptReader struct {
 }
 
 func (pr *PromptReader) ReadString() (string, error) {
+	return pr.promptForStrValue(func() (string, error) {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			return scanner.Text(), nil
+		}
+
+		return "", scanner.Err()
+	})
+}
+
+func (pr *PromptReader) ReadPassword() (string, error) {
+	return pr.promptForStrValue(func() (string, error) {
+		inputBytes, err := ReadPassword()
+		return string(inputBytes), err
+	})
+}
+
+func (pr *PromptReader) promptForStrValue(reader func() (string, error)) (string, error) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	msgChan := make(chan string, 1)
 	errChan := make(chan error, 1)
 	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			msgChan <- scanner.Text()
-			return
-		}
-		if err := scanner.Err(); err != nil {
+		msg, err := reader()
+		if err != nil {
 			errChan <- err
 		}
+
+		msgChan <- msg
 	}()
 
 	select {
@@ -35,9 +51,4 @@ func (pr *PromptReader) ReadString() (string, error) {
 	case err := <-errChan:
 		return "", err
 	}
-}
-
-func (pr *PromptReader) ReadPassword() (string, error) {
-	inputBytes, err := ReadPassword()
-	return string(inputBytes), err
 }
